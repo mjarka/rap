2daf2b333a649ccba73c0015eeea48a848d5fa02
Marcin <marcinjarka+heroku@gmail.com>
2022-11-14 13:57:57 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import useSound from "use-sound";
+import door from "./audio/drzwi.m4a";
+import { useEffect, useState } from "react";
+import Beeper from "./Beeper";
 
 function App() {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("This will run every second!");
+      setCount(2);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
+        <Beeper counter={count} />
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ src/Beeper.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import useSound from "use-sound";
+import beep from "./audio/drzwi.m4a";
+
+export default function Sound({ counter }) {
+  const [play] = useSound(beep, { interrupt: true });
+
+  useEffect(() => {
+    play();
+  }, [counter, play]);
+  return null;
+}
